f234523afdffc04a09d5d2161362c44fc73c5c33
Uddhav Narayan Bhat <bhatuddhav@gmail.com>
2024-03-19 19:41:39 +0530

    comminigt

--- little_lemon/src/App.js
+++ little_lemon/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Nav from './components/Nav';
 function App() {
-  return (true);
+  return(
+    <div className='container'>
+      <Nav/>
+    </div>
+  )
 }
 
 export default App;
