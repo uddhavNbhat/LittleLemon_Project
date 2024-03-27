8b1b3b8076b6bd0fdc761130d5262a5e3d2267c8
Uddhav Narayan Bhat <bhatuddhav@gmail.com>
2024-03-27 22:42:01 +0530

    commit

--- little_lemon/src/App.test.js
+++ little_lemon/src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Booking from './components/Booking';
+import { submitAPI } from './utils/temp';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders the BookingForm heading', () => {
+  render(<Booking />);
+  const headingElement = screen.getByText("FIND A TABLE");
+  expect(headingElement).toBeInTheDocument();
+})
+
+test('submitAPI returns true', () => {
+  const formData = {
+    date: '2022-10-12',
+    time: '20:00',
+    guests: '5',
+    occasion: 'Birthday',
+  };
+  const result = submitAPI(formData);
+  expect(result).toBe(true);
 });
